a9ac2abddb2ae79a9503fd828dc28a863dcb93c6
caitobrien <csobrien@uw.edu>
2023-12-21 15:49:51 -0800

    adding statement to allow reactive switch of DOY vs Temp for SAR plot.. add to TI plot.

--- R/app_server.R
+++ R/app_server.R
@@ -7,12 +7,12 @@
 app_server <- function(input, output, session) {
   mod_Dashboard_server("dashboard_text_1")
 
-  Pdata <- reactive(mod_dataselect_server("dataselect_1"))
+  filtered_data <- reactive(mod_dataselect_server("dataselect_1"))
 
-  mod_SAR_plot_server("SAR_plot_1", Pdata())
-  mod_SAR_table_server("SAR_table_1", Pdata())
+  mod_SAR_plot_server("SAR_plot_1", filtered_data())
+  mod_SAR_table_server("SAR_table_1", filtered_data())
 
-  mod_TI_plot_server("TI_plot_1", Pdata())
+  mod_TI_plot_server("TI_plot_1", filtered_data())
 
   mod_Background_server("Background_1")
 
