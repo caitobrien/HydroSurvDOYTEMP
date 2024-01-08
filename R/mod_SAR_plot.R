8e5e1fc7cbddb365f6f6c3a54db25fb71d6cfd27
caitobrien <csobrien@uw.edu>
2024-01-08 11:05:37 -0800

    add example code

--- R/mod_SAR_plot.R
+++ R/mod_SAR_plot.R
@@ -19,28 +19,28 @@ mod_SAR_plot_ui <- function(id){
 #' SAR_plot Server Functions
 #'
 #' @noRd
-mod_SAR_plot_server <- function(id, data, year_display){
+mod_SAR_plot_server <- function(id, data){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
 
     # Retrieve reactive values from mod_dataselect_server
-    dataselect_reactives <- mod_dataselect_server("dataselect_1")
-    filtered_data <- dataselect_reactives$filtered_data
-    year_display <- dataselect_reactives$year_display
+    # dataselect_reactives <- mod_dataselect_server("dataselect_1")
+    # filtered_data <- dataselect_reactives$filtered_data
+    # year_display <- dataselect_reactives$year_display
 
     output$SAR_plot <- plotly::renderPlotly({
 
       # Filter data based on user selection
-      if (year_display() == "All Years") {
+      # if (year_display() == "All Years") {
 
-        ggplot(filtered_data(), aes(x=x_var, y=SAR))+
-          geom_point()
+        ggplotly(ggplot(data(), aes(x=x_var, y=SAR))+
+          geom_point())
 
-      } else if (year_display() == "Year") {
-
-        ggplot(filtered_data(), aes(x=x_var, y=SAR))+
-          geom_point()
-       }
+      # } else if (year_display() == "Year") {
+      #
+      #   ggplot(filtered_data(), aes(x=x_var, y=SAR))+
+      #     geom_point()
+      #  }
     })
   })
 }
