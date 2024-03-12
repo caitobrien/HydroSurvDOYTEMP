e97f32081d515f4cacbd6713e80c92914f2613b9
caitobrien <csobrien@uw.edu>
2024-03-12 09:19:31 -0700

    add pkg to fct_sar_all

--- R/fct_SAR_all_years_plot.R
+++ R/fct_SAR_all_years_plot.R
@@ -12,31 +12,30 @@ fct_SAR_all_years_plot <- function(data) {
 
   # wrangle data to plot median per year per grouping
   data_summarized <- data %>%
-    #filter(covariate == "Day-of-year (DOY)") %>%
-    mutate(x_var = case_when(
+    dplyr::mutate(x_var = dplyr::case_when(
       covariate == "Day-of-year (DOY)" ~ doy,
       TRUE ~ temp
     )) %>%
-    mutate(
+    dplyr::mutate(
       transport = as.factor(transport),
       year = as.factor(year),
       rear_type = as.factor(rear_type),
       covariate = as.factor(covariate),
       species = as.factor(species)
     ) %>%
-    group_by(covariate, x_var, species, rear_type, transport, doy) %>%
-    summarise(SAR.median = median(SAR, na.rm = TRUE),
-              sd.SAR = sd(SAR, na.rm = TRUE),
-              n.SAR = n(),
-              sar.pit.median = median(sar.pit, na.rm = TRUE),
-              sd.sar.pit = sd(SAR, na.rm = TRUE),
-              n.sar.pit = n()) %>%
-    mutate(se.SAR = sd.SAR / sqrt(n.SAR),
-           SAR.lower = SAR.median - qnorm(0.975) * se.SAR,
-           SAR.upper = SAR.median + qnorm(0.975) * se.SAR,
+    dplyr::group_by(covariate, x_var, species, rear_type, transport, doy) %>%
+    dplyr::summarise(SAR.median = stats::median(SAR, na.rm = TRUE),
+              sd.SAR = stats::sd(SAR, na.rm = TRUE),
+              n.SAR = dplyr::n(),
+              sar.pit.median = stats::median(sar.pit, na.rm = TRUE),
+              sd.sar.pit = stats::sd(SAR, na.rm = TRUE),
+              n.sar.pit = dplyr::n()) %>%
+    dplyr::mutate(se.SAR = sd.SAR / sqrt(n.SAR),
+           SAR.lower = SAR.median - stats::qnorm(0.975) * se.SAR,
+           SAR.upper = SAR.median + stats::qnorm(0.975) * se.SAR,
            se.sar.pit = sd.sar.pit / sqrt(n.sar.pit),
-           sar.pit.lower = sar.pit.median - qnorm(0.975) * se.sar.pit,
-           sar.pit.upper = sar.pit.median + qnorm(0.975) * se.sar.pit)
+           sar.pit.lower = sar.pit.median - stats::qnorm(0.975) * se.sar.pit,
+           sar.pit.upper = sar.pit.median + stats::qnorm(0.975) * se.sar.pit)
 
   # Convert data_summarized to data frame
   data_summarized <- as.data.frame(data_summarized)
@@ -45,8 +44,8 @@ fct_SAR_all_years_plot <- function(data) {
   covar_label <- unique(data_summarized$covariate)
 
   # plot
-  p <- ggplot(data_summarized, aes(x = x_var, color = transport)) +
-    labs(
+  p <- ggplot2::ggplot(data_summarized, ggplot2::aes(x = x_var, color = transport)) +
+    ggplot2::labs(
       x = covar_label,
       y = "Smolt-to-Adult Ratio\n(SAR)",
       shape = "Observed data",
@@ -54,10 +53,10 @@ fct_SAR_all_years_plot <- function(data) {
       fill = "Predicted SAR",
       title = NULL
     ) +
-    geom_point(aes(y = SAR.median, fill = transport)) +
-    geom_line(aes(y = SAR.median)) +
-    geom_ribbon(aes(y = SAR.median, ymin = SAR.lower, ymax = SAR.upper, fill = transport), alpha = .25) +
-    ggdist::geom_pointinterval(aes(
+    ggplot2::geom_point(ggplot2::aes(y = SAR.median, fill = transport)) +
+    ggplot2::geom_line(ggplot2::aes(y = SAR.median)) +
+    ggplot2::geom_ribbon(ggplot2::aes(y = SAR.median, ymin = SAR.lower, ymax = SAR.upper, fill = transport), alpha = .25) +
+    ggdist::geom_pointinterval(ggplot2::aes(
       y = ifelse(n.sar.pit > 7, sar.pit.median, NA),
       ymin = sar.pit.lower,
       ymax = sar.pit.upper,
@@ -65,25 +64,25 @@ fct_SAR_all_years_plot <- function(data) {
       color = transport),
       alpha = .25
     ) +
-    scale_color_manual(
+    ggplot2::scale_color_manual(
       breaks = c("0", "1"),
       values = c("steelblue4", "#b47747"),
       labels = c("In-river, \nmedian with 95% CI", "Transported, \nmedian with 95% CI")
     ) +
-    scale_fill_manual(
+    ggplot2::scale_fill_manual(
       breaks = c("0", "1"),
       values = c("steelblue4", "#b47747"),
       labels = c("In-river, \nmedian with 95% CI", "Transported, \nmedian with 95% CI")
     ) +
-    scale_shape_manual(
+    ggplot2::scale_shape_manual(
       values = c(21, 21),
       breaks = c("0", "1"),
       labels = c("In-river,\nmedian per year", "Transported,\nmedian per year")
     ) +
-    theme_light() +
-    facet_grid(rear_type ~ species, scales = "free_y") +
-    theme(strip.background = element_rect(fill = "lightgrey")) +
-    theme(strip.text = element_text(colour = "black"))
+    ggplot2::theme_light() +
+    ggplot2::facet_grid(rear_type ~ species, scales = "free_y") +
+    ggplot2::theme(strip.background = ggplot2::element_rect(fill = "lightgrey")) +
+    ggplot2::theme(strip.text = ggplot2::element_text(colour = "black"))
 
   p
 }
