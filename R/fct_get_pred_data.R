079522f5b62c2200dcf5048c7403946bd91eddf6
caitobrien <csobrien@uw.edu>
2024-03-12 09:05:01 -0700

    remove fct_get data -- try another method

--- R/fct_get_pred_data.R
+++
@@ -1,9 +0,0 @@
-#' get_pred_data
-#'
-#' @description A fct function
-#'
-#' @return The return value, if any, from executing the function.
-#'
-#' @noRd
-
-data<-read.csv(here::here("data", "df_mod_predict.csv"))
