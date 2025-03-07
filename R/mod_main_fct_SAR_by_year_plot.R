35fd827fa932a11c92f36a674697bf15839c3d66
caitobrien <csobrien@uw.edu>
2025-03-06 16:01:59 -0800

    changed to match all_years format-- could condense function? now works with list and sep pred/obs & doy/temp lists

--- R/mod_main_fct_SAR_by_year_plot.R
+++ R/mod_main_fct_SAR_by_year_plot.R
@@ -8,47 +8,32 @@
 
 fct_SAR_by_year_plot<-function(data, observed_data, selected_years, selected_covariate, observed = "no"){
 
-x_var <- if (selected_covariate == "Day-of-year (DOY)") {
-    "doy"
-  } else if (selected_covariate == "Temperature (°C)") {
-    "mean.temp"
-  } else {
-    stop("Invalid covariate selected")
-  }
-print(data)
+  if (observed == "no") {
 
-  #wrangle data to plot median per year per grouping
-  data_summarized<- data %>%
-    # dplyr::mutate(x_var = dplyr::case_when(
-    #   covariate == "Day-of-year (DOY)" ~ doy,
-    #   TRUE ~ mean.temp#temp
-    # )) %>%
-    dplyr::mutate(
-      transport = as.factor(transport),
-      year = as.factor(year),
-      rear_type = as.factor(rear_type),
-      covariate = as.factor(covariate),
-      species = as.factor(species),
-      species_rear = interaction(species, rear_type)) %>%
-    dplyr::group_by(year)
+    if (selected_covariate == "Day-of-year (DOY)") {
+      data_summarized<- data %>%
+        dplyr::mutate(species_rear = interaction(species, rear_type)) %>%
+        dplyr::group_by(year)
 
-  # Convert data_summarized to data frame
-  data_summarized <- as.data.frame(data_summarized)
+      x_var <- data_summarized$doy
+      covar_label <- "Day-of-year (DOY)"
 
-  # # Extract unique covariate name
-  # covar_label <-  unique(data_summarized$covariate)
+    } else if (selected_covariate == "Temperature (°C)") {
+      data_summarized<- data %>%
+        dplyr::mutate(species_rear = interaction(species, rear_type)) %>%
+        dplyr::group_by(year)
 
-
+      x_var <- data_summarized$mean.temp
+      covar_label <- "Temperature (°C)"
+    }
 
   p <-
     ggplot2::ggplot(data_summarized, ggplot2::aes( x= x_var)) +
     ggplot2::geom_point(ggplot2::aes(y =SAR, fill =  transport, color = transport))+
-    # ggplot2::geom_line(ggplot2::aes(y = SAR)) +
-    # ggplot2::geom_ribbon( ggplot2::aes(y = SAR, ymin =SAR.lo, ymax = SAR.hi, fill =  transport, color = transport), alpha = .25)+
     tidybayes::geom_lineribbon(ggplot2::aes(y = SAR, ymin = SAR.lo, ymax = SAR.hi, fill = transport, color = transport),
                                alpha = .25
     ) +
-    ggplot2::labs( x = selected_covariate,
+    ggplot2::labs( x = covar_lab,
           y = "Smolt-to-Adult Ratio\n(SAR)",
           size = "Number of fish observed",
           color = "Predicted SAR",
@@ -79,51 +64,106 @@ print(data)
           panel.spacing = ggplot2::unit(2, "lines"),
           panel.grid.minor = ggplot2::element_blank(),
           text = ggplot2::element_text(size = 15))
+  } else if (observed == "yes") {
 
 
-  #functionality to include obs data or not in plot
-  if (observed == "yes") {
-    if(selected_covariate == "Day-of-year (DOY)"){
-      observed_data<- observed_data %>% dplyr::mutate(covariate = "Day-of-year (DOY)")
-    } else if(selected_covariate == "Temperature (°C)"){
-      observed_data<- observed_data %>% dplyr::mutate(covariate = "Temperature (°C)")
+    if (selected_covariate == "Day-of-year (DOY)") {
+      #predicted data
+      data_summarized<- data %>%
+        dplyr::mutate(species_rear = interaction(species, rear_type)) %>%
+        dplyr::group_by(year)
+
+      x_var <- data_summarized$doy
+      covar_label <- "Day-of-year (DOY)"
+
+      #observed data
+      wrangled_observed_data <- observed_data %>%
+        dplyr::mutate(species_rear = interaction(species, rear_type)) %>%
+        dplyr::group_by(year) %>%
+        filter(n > 7) #remove data points with less than 7 observations
+
+      x_var_obs<- wrangled_observed_data$doy
+
+      x_breaks <- seq(90, 160, by = 10)
+
+    } else if (selected_covariate == "Temperature (°C)") {
+
+      #prediction data
+      data_summarized<- data %>%
+        dplyr::mutate(species_rear = interaction(species, rear_type)) %>%
+        dplyr::group_by(year)
+
+      x_var <- data_summarized$mean.temp
+      covar_label <- "Temperature (°C)"
+
+      #observed data
+      wrangled_observed_data <- observed_data %>%
+        dplyr::mutate(species_rear = interaction(species, rear_type)) %>%
+        dplyr::group_by(year) %>%
+        filter(n > 7) #remove data points with less than 7 observations
+
+      x_var_obs<- wrangled_observed_data$mean.temp
+
+      x_breaks <- seq(6, 18, by = 2)
+
     }
 
-    wrangled_observed_data<-observed_data %>%
-      # dplyr::mutate(x_var = dplyr::case_when(
-      #   covariate == "Day-of-year (DOY)" ~ doy,
-      #   TRUE ~ mean.temp#temp
-      # )) %>%
-      dplyr::mutate(
-        transport = as.factor(transport),
-        year = as.factor(year),
-        rear_type = as.factor(rear_type),
-        covariate = as.factor(covariate),
-        species = as.factor(species),
-        species_rear = interaction(species, rear_type)) %>%
-      dplyr::group_by(year)
 
-    p.obs <- p +  ggplot2::geom_point(data = wrangled_observed_data,
-                                      ggplot2::aes(
-                                        x = x_var,
-                                        y =sar.pit,
-                                        size = n,
-                                        shape =  transport,
-                                        color = transport),
-                                      alpha = .7)+
-      ggplot2::labs(shape = "Observed data") +
+    p <-
+      ggplot2::ggplot() +
+
+      ggplot2::geom_point( data = data_summarized, ggplot2::aes( x = x_var, y =SAR, fill =  transport, color = transport))+
+      tidybayes::geom_lineribbon(data = data_summarized, ggplot2::aes(x = x_var,y = SAR, ymin = SAR.lo, ymax = SAR.hi, fill = transport, color = transport),
+                                 alpha = .25
+      ) +
+      ggplot2::geom_point(data = wrangled_observed_data,
+                          ggplot2::aes(
+                            x = x_var_obs,
+                            y =sar.pit,
+                            size = n,
+                            shape =  transport,
+                            color = transport),
+                          alpha = .7)+
+      ggplot2::labs( x = covar_label,
+                     y = "Smolt-to-Adult Ratio\n(SAR)",
+                     size = "Number of fish observed",
+                     color = "Predicted SAR",
+                     fill = "Predicted SAR",
+                     shape = "Observed data",
+                     title = NULL
+      ) +
+      ggplot2::scale_color_manual(breaks = c("0", "1"),
+                                  values = c("steelblue4", "#b47747"),
+                                  labels = c("In-river, \npredicted with 95% CI", "Transported, \npredicted with 95% CI"))+
+      ggplot2::scale_fill_manual(breaks = c("0", "1"),
+                                 values = c("steelblue4", "#b47747"),
+                                 labels = c("In-river, \npredicted with 95% CI", "Transported, \npredicted with 95% CI"))+
+      ggplot2::scale_linetype_manual(values = c("solid","dashed"),
+                                     breaks = c("0", "1"),
+                                     labels = c("In-river,\nmedian predicted probability", "Transported,\nmedian predicted probability"))+
       ggplot2::scale_shape_manual(values = c(21,21),
                                   breaks = c("0", "1"),
                                   labels = c("In-river", "Transported")) +
       ggplot2::scale_size_continuous(range = c(1, 5),
-                                     breaks = c(1, pretty(c(1, max(stats::na.omit(wrangled_observed_data$n)), n = 3))))
+                                     breaks = c(10, pretty(c(10, max(stats::na.omit(wrangled_observed_data$n)), n = 3)))) +
+      ggplot2::guides(shape = ggplot2::guide_legend(override.aes = list(color = c("steelblue4", "#b47747") ),
+                                                    order = 1),
+                      size = ggplot2::guide_legend(override.aes = list(
+                        label = list(size = 8),
+                        shape = 21),
+                        order = 2),
+                      color = ggplot2::guide_legend(order = 3),
+                      fill = ggplot2::guide_legend(order = 3)) +
+      ggplot2::theme_light()+
+      ggplot2::facet_wrap(~year + species_rear, scales = "free_y", ncol = 4) +
+      ggplot2::theme(strip.background = ggplot2::element_rect(fill="lightgrey"),
+                     strip.text = ggplot2::element_text(colour = 'black'),
+                     panel.spacing = ggplot2::unit(2, "lines"),
+                     panel.grid.minor = ggplot2::element_blank(),
+                     text = ggplot2::element_text(size = 15))
 
-
-    return(p.obs)
-
-  } else {
-    return(p)
   }
+    return(p)
 
 }
 
